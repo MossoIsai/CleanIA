8f9ab5e9d6e6416581573e96ca33988cb9311665
Mosso <ing.isaimosso@gmail.com>
2019-06-21 11:00:15 -0500

    [FEATURE] first commit

---
+++ app/src/main/java/com/mosso/cleania/CleanIAApp.kt
@@ -0,0 +1,11 @@
+package com.mosso.cleania
+
+import android.app.Application
+
+
+class CleanIAApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/mosso/cleania/login/LoginActivity.kt
@@ -0,0 +1,6 @@
+package com.mosso.cleania.login
+
+
+class LoginActivity {
+
+}
